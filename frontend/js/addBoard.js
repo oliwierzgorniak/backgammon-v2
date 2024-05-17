cc02ba5a813c715ccc86c95d58caf2a0e148c4be
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2024-05-17 13:35:59 +0200

    segments positioned

--- frontend/js/addBoard.js
+++ frontend/js/addBoard.js
@@ -1,8 +1,20 @@
 import Store from "./Store.js";
 import Base from "./addBoard/Base.js";
 import Segment from "./addBoard/Segment.js";
+import { BENCH_WIDTH, SEGMENT_WIDTH } from "./addBoard/consts.js";
 
 export default function addBoard() {
   Store.scene.add(new Base());
-  Store.scene.add(new Segment(0, 0));
+
+  for (const z of [-1, 1]) {
+    for (let x = -6; x <= 6; x++) {
+      const xPosition =
+        SEGMENT_WIDTH * x +
+        ((BENCH_WIDTH / 2 - SEGMENT_WIDTH / 2) * x) / Math.abs(x);
+
+      const zPosition = 0;
+
+      Store.scene.add(new Segment(xPosition, zPosition, z == 1));
+    }
+  }
 }
--- frontend/js/addBoard/Segment.js
+++ frontend/js/addBoard/Segment.js
@@ -1,21 +1,32 @@
 import * as THREE from "../../libs/three.module.min.js";
-import { COLOR_BOARD_DARK, SEGMENT_HEIGHT, SEGMENT_WIDTH } from "./consts.js";
+import {
+  BOARD_DEPTH,
+  COLOR_BOARD_DARK,
+  HORIZONTAL_GAP,
+  SEGMENT_HEIGHT,
+  SEGMENT_WIDTH,
+} from "./consts.js";
 import Triangle from "./Triangle.js";
 
 export default class Segment extends THREE.Object3D {
-  constructor(x, y) {
+  constructor(x, z, isRotated = false) {
     super();
 
     this.geometry = new THREE.PlaneGeometry(SEGMENT_WIDTH, SEGMENT_HEIGHT);
     this.material = new THREE.MeshStandardMaterial({
-      color: COLOR_BOARD_DARK,
+      color: 0xffffff,
     });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
-    const triangle = new Triangle(x, y);
-    triangle.position.y = 10;
-    console.log(triangle.position);
+    const triangle = new Triangle(0, 0);
+
+    this.position.x = x;
+    this.position.z = z + SEGMENT_HEIGHT / 2 + HORIZONTAL_GAP / 2;
 
     this.rotation.x = -(Math.PI * 2) / 4;
+    if (isRotated) {
+      this.rotation.z = (Math.PI * 2) / 2;
+      this.position.z -= SEGMENT_HEIGHT + HORIZONTAL_GAP;
+    }
     this.position.y = 0.5;
 
     this.add(triangle.mesh); // Add the mesh to the object
